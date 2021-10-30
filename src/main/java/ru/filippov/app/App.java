a16a71169a6e910a4d00e051a0a838f3ef08d810
filip <filippov.alex1999@yandex.ru>
2021-10-30 20:44:17 +0400

    Try fix heroku

--- src/main/java/ru/filippov/app/App.java
+++ src/main/java/ru/filippov/app/App.java
@@ -1,12 +1,20 @@
 package ru.filippov.app;
 
+import org.openapitools.client.api.MortgageCalculatorApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
+    }
+
+    @Bean
+    public MortgageCalculatorApi transferService() {
+        
+        return new MortgageCalculatorApi();
     }
 
 }
